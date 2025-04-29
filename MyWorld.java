c6e346e892040989e328ed504ef02376c53e51f5
edwardw878 <edwardwang878@gmail.com>
2025-04-29 13:59:22 -0400

    Added elephant and apple

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,19 @@
 import greenfoot.*;
 
+/**
+ * The world our hero lives in.
+ * 
+ * @author Edward Wang
+ * version April 2025
+ */
+
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
+        
+        Apple apple = new Apple();
+        addObject(apple, 300, 0);
     }
 }
