a30b81704e5b0dae62e4c046fa59d179d0171fb3
edwardw878 <edwardwang878@gmail.com>
2025-04-29 18:34:01 -0400

    Added random apple spawn locations

--- MyWorld.java
+++ MyWorld.java
@@ -13,7 +13,18 @@ public class MyWorld extends World {
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
         
+        createApple();
+        
+    }
+    
+    /**
+     * Creates a new apple at random location at the top
+     */
+    public void createApple()
+    {
         Apple apple = new Apple();
-        addObject(apple, 300, 0);
+        int x = Greenfoot.getRandomNumber(600); 
+        int y = 0;
+        addObject(apple, x, y);    
     }
 }
