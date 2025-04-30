ce2293a56c131523e163123133a000f8890e3ae0
edwardw878 <edwardwang878@gmail.com>
2025-04-30 14:14:17 -0400

    added score

--- MyWorld.java
+++ MyWorld.java
@@ -8,13 +8,34 @@ import greenfoot.*;
  */
 
 public class MyWorld extends World {
+    public int score = 0;
+    Label scoreLabel;
+    
+    /**
+     * Constructor for objects of class MyWorld.
+     */
     public MyWorld() {
         super(600, 400, 1);
+        //Creates elephant object
         Elephant elephant = new Elephant();
-        addObject(elephant, 300, 300);
+        addObject(elephant, 300, 300); 
         
+        //Creates label
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
+        
+        //Creates apple
         createApple();
         
+    }
+    
+    /**
+     * Increase score
+     */
+    public void increaseScore()
+    {
+        score++; 
+        scoreLabel.setValue(score);
     }
     
     /**
