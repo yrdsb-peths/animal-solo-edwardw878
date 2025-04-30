da0e831f6f55867bf7665c1bd493f9825b573ed4
edwardw878 <edwardwang878@gmail.com>
2025-04-29 20:40:09 -0400

    added eat method

--- MyWorld.java
+++ MyWorld.java
@@ -11,7 +11,7 @@ public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
         Elephant elephant = new Elephant();
-        addObject(elephant, 300, 200);
+        addObject(elephant, 300, 300);
         
         createApple();
         
